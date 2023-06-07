8903a1696f90767451cbbf25109e98938ae0e681
Erick <efmz2000@outlook.es>
2023-06-07 15:24:59 -0600

    base

---
+++ helm/encript.sh
@@ -0,0 +1,6 @@
+echo 'storageaccount: '
+echo 'filesmanagerfta' | base64
+echo 'key: '
+echo 'l/hyKP5OC7THsPr0FvvNJWBg14+JGyFnbH++OwpEcfVEtV0+R0SJPM56EfTwY/NUfHbOZ686BWkT+AStardlVQ==' | base64
+echo 'container: '
+echo 'DefaultEndpointsProtocol=https;AccountName=filesmanagerfta;AccountKey=l/hyKP5OC7THsPr0FvvNJWBg14+JGyFnbH++OwpEcfVEtV0+R0SJPM56EfTwY/NUfHbOZ686BWkT+AStardlVQ==;EndpointSuffix=core.windows.net    ' | base64
