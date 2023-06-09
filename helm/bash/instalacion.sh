8c6a9bb8df6823660df6903fa1f5997ce1074ce0
Erick <efmz2000@outlook.es>
2023-06-09 15:23:34 -0600

    pequeños cambios

--- helm/bash/instalacion.sh
+++ helm/bash/instalacion.sh
@@ -1,9 +1,14 @@
+echo "Instalando bootstrap"
 helm install boostrap boostrap
 
+echo "Instalando databases"
 helm dependency update databases
 helm dependency build databases
 helm install databases databases
 
-helm install bakcups backups
+echo "Install backups" 
+helm dependency update backups
+helm dependency build backups
+helm install backups backups
 
 
