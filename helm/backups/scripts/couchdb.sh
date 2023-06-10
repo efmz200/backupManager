90317725f065ae23f46c0b415d2e95e3c26cdde8
Erick <efmz2000@outlook.es>
2023-06-10 01:41:38 -0600

    cambios en las bases y backups

--- helm/backups/scripts/couchdb.sh
+++ helm/backups/scripts/couchdb.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 DATE=$(date '+%Y%m%d%H%M')
-mkdir -p /couchdbdump/$DATE
+mkdir -p /couchdbbackup/$DATE
 apk update
 apk upgrade
-apk add  curl
+apk add curl zip
 az config set extension.use_dynamic_install=yes_without_prompt
-curl -X POST http://databases-couchdb.default.svc.cluster.local:5984/_replicate -H "Content-Type: application/json" -d '{
-  "source": "http://localhost:5984",
-  "target": "/couchdbdump/'$DATE'",
-  "create_target": true
-}'
-az storage blob directory upload --container $CONTAINER -s /couchdbdump/$DATE -d $BACKUP_PATH --auth-mode key --recursive
-rm -rf /couchdbdump/$DATE
+echo "Starting backup"
+
+for i in {0..2}; do
+  db_pod="databases-couchdb-$i"
+  echo "Backing up database $db_pod"
+
+  # Perform backup of the database in the current pod
+  curl -s -u "$COUCHDB_USERNAME:$COUCHDB_PASSWORD" -X GET http://$db_pod:$COUCHDB_PORT > /couchdbbackup/$DATE/$db_pod.json
+
+  # Check if the backup was successful
+  if [ $? -eq 0 ]; then
+    echo "Backup of database $db_pod completed successfully"
+  else
+    echo "Backup of database $db_pod failed"
+  fi
+done
+
+# Upload the compressed archive to Azure Blob storage
+az storage blob directory upload --container $CONTAINER -s /couchdbbackup/$DATE -d $BACKUP_PATH --auth-mode key --recursive
+
+
+# Remove the local backup directory and archive
+rm -rf /couchdbbackup/$DATE
+
+
+echo "Backup process completed"
