2b986ee00fa9c87b6c391b3d143550afbf4ac6fa
Erick <efmz2000@outlook.es>
2023-06-13 13:08:42 -0600

    cambios para maria

--- helm/backups/scripts/mariadb.sh
+++ helm/backups/scripts/mariadb.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
 DATE=$(date '+%Y%m%d%H%M')
 mkdir -p /mariadbbackup/$DATE
+
 apk update
 apk upgrade
-apk add mariadb-client
+apk add mariadb-backup
+
 az config set extension.use_dynamic_install=yes_without_prompt
-mysqldump --host="$MYSQL_HOST" -u $MYSQL_USERNAME -p$MYSQL_PASSWORD --databases $MYSQL_DATABASE --single-transaction --compress --result-file=/mariadbbackup/$DATE/database.sql
+
+mariabackup --backup \
+   --target-dir=/mariadbbackup/$DATE/ \
+   --user=$MARIADB_USERNAME --password=$MARIA_PASSWORD --port=$MARIA_PORT --host=$MARIADB_HOST
+
+# mysqldump --host="$MYSQL_HOST" -u $MYSQL_USERNAME -p$MYSQL_PASSWORD --databases $MYSQL_DATABASE --single-transaction --compress --result-file=/mariadbbackup/$DATE/database.sql
+
 az storage blob directory upload --container $CONTAINER -s /mariadbbackup/$DATE -d $BACKUP_PATH --auth-mode key --recursive
+
 rm -rf /mariadbbackup/$DATE
