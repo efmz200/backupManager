941d63b61575e5eeb07946ba6a40ff45567fe8a1
Erick <efmz2000@outlook.es>
2023-06-14 13:55:10 -0600

    azure data

--- helm/bash/azure/encript.sh
+++ helm/bash/azure/encript.sh
@@ -1,6 +1,6 @@
 echo 'storageaccount: '
-echo 'orantoonstorageaccount' | base64
+echo 'backupsp3bd2' | base64
 echo 'key: '
-echo 'j+oZmgHnYWULV7HlyBS/dhZakjZilUcrJz/GE/t52jFYIIf9HlorcrTnDFKbFIO5JX+MabTwRB+0+AStcEn+VQ==' | base64
+echo '1GYqfrwQoqUFfRsb8IQDql2WjzQqwfUg07iSK0tXpztFZYIlNlvlK9blbdBleBX02GI2mqZYMHsS+AStfUkqPw==' | base64
 echo 'container: '
 echo 'DefaultEndpointsProtocol=https;AccountName=orantoonstorageaccount;AccountKey=j+oZmgHnYWULV7HlyBS/dhZakjZilUcrJz/GE/t52jFYIIf9HlorcrTnDFKbFIO5JX+MabTwRB+0+AStcEn+VQ==;EndpointSuffix=core.windows.net' | base64
