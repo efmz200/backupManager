d41ebdf520660340338e6180e5d09b92844e210e
Erick <efmz2000@outlook.es>
2023-06-15 21:30:58 -0600

    mongo y maria restore

--- helm/restores/scripts/mariadb.sh
+++ helm/restores/scripts/mariadb.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-mkdir -p /mariadump/$DATE
 apk update
 apk upgrade
 apk add mysql-client
 az config set extension.use_dynamic_install=yes_without_prompt
 
 mkdir -p /mariarestore/$DATE
+az storage blob directory download -c "$CONTAINER" --account-name "$AZURE_STORAGE_ACCOUNT" -s mariaBackup -d /mariarestore --recursive --verbose --auth-mode key
+mysqldump -u "$MARIA_USERNAME" -p"$MARIA_PASSWORD" --host="$MARIADB_HOST" --port="$MARIA_PORT" --all-databases < "/mariarestore/mariaBackup/$DATE/backup.sql"
+exit_code=$?
+echo "Código de salida: $exit_code"
 
-az storage blob directory download --container $CONTAINER -s /mariarestore/$DATE --file "$DESTINATION_PATH" --auth-mode key
-
-mysqldump -u "$MARIA_USERNAME" -p"$MARIA_PASSWORD" --host="$MARIADB_HOST" --port="$MARIA_PORT" --all-databases > "/mariadump/$DATE/backup.sql"
-
-rm -rf /mariarestore/$DATE
+rm -rf /mariarestore
